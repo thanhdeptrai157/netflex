bd1fd9d1814dc95e94a57b56b3e988cf9f4b57e3
Thanh <thanhcap3@gmail.com>
2025-02-16 21:22:12 +0700

    feat: api setup, services

---
+++ src/config/env.ts
@@ -0,0 +1,4 @@
+
+// environment variables
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
---
+++ src/services/categoryService.ts
@@ -0,0 +1,25 @@
+import axios from '@/config/axios';
+
+// get all categories
+export const getAllCategories = async () => {
+    try {
+        const response = await axios.get('the-loai');
+        return response.data;
+    }
+    catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+// get movie by category
+export const getMoviesByCategory = async (slug: string) => {
+    try {
+        const response = await axios.get(`v1/api/the-loai/${slug}`);
+        return response.data;
+    }
+    catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
